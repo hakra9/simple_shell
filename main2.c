6d4c7f10a24fe37123fe38b6d56ea442c881812e
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-08-23 00:37:10 +0200

    mandatory tasks

--- main2.c
+++ main2.c
@@ -9,22 +9,19 @@ int main(int argc, char **argv, char **env)
 {
     char *buf = NULL;
     size_t buf_size = 0;
-    char *token;
     char **array;
     int n_char;
-    char *prog_name;
+    char *prog_name, *temp;
     int mode;
-    char msg_pgn[300];
 
     prog_name = argv[0];
-    if (argc > 1)
-        mode = 0;
-    else{
-        mode = 1;
-    }    
-    while (1)
-    {
-        if (mode == 1)
+    if (isatty(fileno(stdin))) {
+        mode = 1; 
+    } else {
+        mode = 0; 
+    }
+    while (1){
+        if (mode == 1 && argc > 0)
             display_prompt();
         n_char = getline(&buf, &buf_size, stdin);
         if (n_char == -1) 
@@ -37,8 +34,14 @@ int main(int argc, char **argv, char **env)
         array = tokenize(buf, buf_size);
         if (strstr(array[0], "/") == NULL){
             if (built_ins(array, env) == 1)
-                array[0] = find_path(env, array [0]);
+                temp = find_path(env, array[0]);
+            if (temp == NULL && mode == 0){
+                printf("%s: 1: %s: not found\n", prog_name, array[0]);
+                break;
             }
+            if (temp != NULL)
+                array[0] = find_path(env, array[0]);
+        }
         forking(array, prog_name, env);
         free(array);
     }
