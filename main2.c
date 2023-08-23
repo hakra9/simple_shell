00d56d8b569d40030d0d2c483c3d313958cc4a8b
hama <hama@example.com>
2023-08-23 15:15:44 -0700

    h

--- main2.c
+++ main2.c
@@ -1,40 +1,66 @@
 #include "main.h"
-/**
-*main - entrance
-*@argc: number of arguments
-*@argv: arrguments
-*@env: environment
-*Return: (0) on sucess
-*/
-int main(int argc, char **argv, char **env) {
-	char *buf = NULL, *prog_name;
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv, char **env)
+{
+	char *buf = NULL;
 	size_t buf_size = 0, length;
 	char **array;
-	int n_char, mode;
-
+	int n_char;
+	char *prog_name, *temp;
+	int mode;
 	prog_name = argv[0];
-	mode = (isatty(fileno(stdin)) ? 1 : 0);
-	while (1) {
+	if (isatty(fileno(stdin)))
+	{
+		mode = 1;
+	}
+	else
+	{
+		mode = 0;
+	}
+	while (1)
+	{
 		if (mode == 1 && argc > 0)
 			display_prompt();
 		n_char = getline(&buf, &buf_size, stdin);
 		if (n_char == -1)
+		{
+		/*write(STDOUT_FILENO, "\n", 1);*/
 			break;
+		}
 		length = strlen(buf);
-		while (length > 0) {
-			if (buf[length - 1] == ' ' || buf[length - 1] == '\t')
-				buf[length - 1] = '\0';
-			if (buf[length - 1] == '\n')
-				buf[length - 1] = '\0';
+		/**if (length > 0 && buf[length - 1] == '\n')
+		 * buf[length - 1] = '\0';
+		 * length--;
+		 * */
+		while (length > 0 && (buf[length - 1] == ' ' || buf[length - 1] == '\t' || buf[length - 1] == '\n'))
+		{
+			buf[length - 1] = '\0';
 			length--;
 		}
 		if (buf[0] == '\0')
 			break;
 		array = tokenize(buf, buf_size);
-		array[0] = check_path(env, array, mode, prog_name);
+		if (strstr(array[0], "/") == NULL)
+		{
+			if (built_ins(array, env) == 1)
+				temp = find_path(env, array[0]);
+			if (temp == NULL && mode == 0)
+			{
+				fprintf(stderr, "%s: 1: %s: not found\n", prog_name, array[0]);
+				exit(127);
+			}
+			if (temp != NULL)
+				array[0] = find_path(env, array[0]);
+		}
 		forking(array, prog_name, env);
 		free(array);
 	}
 	free(buf);
 	return (0);
 }
+
